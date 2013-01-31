ced95f68b9e0c9621dff399b6ec6cd035c9a8291
Haja Rambelontsalama <bensmania@gmail.com>
2013-01-31 11:01:54 +0100

    wait for scalaskel question

--- web/src/main/java/com/hrambelo/codestory/web/controller/CodestoryController.java
+++ web/src/main/java/com/hrambelo/codestory/web/controller/CodestoryController.java
@@ -1,7 +1,6 @@
 package com.hrambelo.codestory.web.controller;
 
 import com.hrambelo.codestory.web.service.manager.factory.ManagerFactory;
-import com.hrambelo.codestory.web.service.scalaskel.ExchangeService;
 import com.hrambelo.codestory.web.service.manager.AFactory;
 import com.hrambelo.codestory.web.service.manager.QuestionManager;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -29,14 +28,5 @@ public class CodestoryController {
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
     public @ResponseBody String handleQuestion(@RequestParam("q") String question){
         return new QuestionManager(factory).configureRoute(question).answer();
-    }
-
-    @RequestMapping(value = "scalaskel/change/{amount}", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody String compute(@PathVariable int amount) throws IOException {
-        List<Object> results = new ExchangeService(amount).exchange();
-        // Mapper json avec option d'inclusion
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_DEFAULT);
-        return mapper.writeValueAsString(results);
     }
 }
